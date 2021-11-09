b5c6d67a2cb04f98030c8dba68a3631e754ba452
Mohit Kochhar <kochhar07@gmail.com>
2021-11-09 20:13:38 +0530

    ready to try deplying

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { isLoggedIn } = require('./middleware');
-const dbUrl = process.env.DB_URL | 'mongodb://localhost:27017/social_app';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/social_app';
 // const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
@@ -44,7 +44,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!',
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
 const store = new MongoStore({
     url: dbUrl,
@@ -61,7 +62,6 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     store: new MongoStore({ mongooseConnection: mongoose.connection })
-
 
 }
 
